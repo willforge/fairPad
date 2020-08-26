6c5553dcb5b1379322fb06ade37f5e921c23a364
Jacqueline R. Teves <jteves@chii.ml>
2020-08-26 17:34:39 +0200

    added auto commit for pad.html

--- dbug2prod.sh
+++ dbug2prod.sh
@@ -1,3 +1,7 @@
 #
 
+date=$(date +%Y-%m-%d)
 git show dbug:pad.html | grep -v -w 'callee' | sed -e 's/DEBUG/MASTER/i' > pad.html
+git add pad.html
+rev=$(git rev-parse --short dbug)
+git commit -m "automatic conversion from dbug $rev on $date"
